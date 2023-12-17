7623245cea21dad75e1f35110f826a113aa11894
olazarcesar <152935847+olazarcesar@users.noreply.github.com>
2023-12-17 17:44:38 -0300

    proyectofinal 17-12-2023

---
+++ script.js
@@ -0,0 +1,40 @@
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+
+function calcular(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+    } );
+    
+}
+
+//Eventos listeners
+monedaEl_one.addEventListener('change', calcular);
+cantidadEl_one.addEventListener('input', calcular);
+monedaEl_two.addEventListener('change', calcular);
+cantidadEl_two.addEventListener('input', calcular);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calcular();
+} );
+
+
+calcular();
